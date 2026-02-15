589b19eabbb5686a2d3f4f50ea613114ce764da8
Fahad <fahad05munir@gmail.com>
2026-02-15 14:44:06 +0500

    edited

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -1,25 +1,24 @@
 import express from "express";
 import multer from "multer";
-import { v2 as cloudinary } from "cloudinary";
+import cloudinary from "../cloudinary.js";
 
 const router = express.Router();
-
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/", upload.single("image"), async (req, res) => {
   try {
     const file = req.file;
 
-    cloudinary.uploader.upload_stream(
-      { folder: "todo-app" },
-      (error, result) => {
-        if (error) return res.status(500).json({ error });
+    if (!file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    cloudinary.uploader
+      .upload_stream({ folder: "todo-app" }, (error, result) => {
+        if (error) return res.status(500).json({ error: error.message });
         res.json({ imageUrl: result.secure_url });
-
-        
-
-      }
-    ).end(file.buffer);
+      })
+      .end(file.buffer);
 
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -27,32 +26,3 @@ router.post("/", upload.single("image"), async (req, res) => {
 });
 
 export default router;
-
-
-
-
-
-// import express from "express";
-// const router = express.Router();
-// const upload = require("../multer");
-// const cloudinary = require("../cloudinary");
-
-// router.post("/upload", upload.single("image"), async (req, res) => {
-//   try {
-//     const result = await cloudinary.uploader.upload_stream(
-//       { folder: "ecommerce" },
-//       (error, result) => {
-//         if (error) return res.status(500).json({ error });
-//         res.json({ imageUrl: result.secure_url });
-//       }
-//     );
-
-//     result.end(req.file.buffer);
-
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
-
-
-// export default router;
